8dd798325653c817b88cc510cbae452ea0adb105
Abdo16j <108068335+Abdo16j@users.noreply.github.com>
2023-06-25 09:24:26 +0100

    Update leavejoin.js

--- src/commands/owner/leavejoin.js
+++ src/commands/owner/leavejoin.js
@@ -1,20 +1,23 @@
 client.on('guildCreate', guild => {
     // اسم البوت الرسمي لديك
     const officialBot = "ProControl";
-    
-    // معرّف السيرفر الرئيسي
-    const mainServerId = "1122241424197427200";
 
-    // معرّف الروم لإرسال رسالة الدخول فيه في السيرفر الرئيسي
+    // معرف الروم في السيرفر الرسمي لإرسال رسالة الدخول
     const joinChannelId = "1122241426445578428";
 
-    if (guild.id === mainServerId) {
-        // العثور على الروم بناءً على المعرّف
-        const joinChannel = guild.channels.cache.get(joinChannelId);
+    // السيرفر الرسمي الذي تود الإعلان عن الانضمام إليه
+    const mainServerId = "1122241424197427200";
 
-        if (joinChannel) {
-            // إرسال رسالة الدخول في الروم المحدد
-            joinChannel.send(`${officialBot} has joined the server.`);
+    // اذا كان السيرفر الذي تمت دعوة البوت اليه هو السيرفر الرسمي
+    if (guild.id !== mainServerId) {
+        const mainServer = client.guilds.cache.get(mainServerId);
+
+        if (mainServer) {
+            const joinChannel = mainServer.channels.cache.get(joinChannelId);
+
+            if (joinChannel) {
+                joinChannel.send(`The bot ${officialBot} has joined a new server: ${guild.name}`);
+            }
         }
     }
 });
@@ -22,20 +25,24 @@ client.on('guildCreate', guild => {
 client.on('guildDelete', guild => {
     // اسم البوت الرسمي لديك
     const officialBot = "ProControl";
-    
-    // معرّف السيرفر الرئيسي
-    const mainServerId = "1122241424197427200";
 
-    // معرّف الروم لإرسال رسالة الخروج فيه في السيرفر الرئيسي
+    // معرف الروم في السيرفر الرسمي لإرسال رسالة الخروج
     const leaveChannelId = "1122241426445578429";
 
-    if (guild.id === mainServerId) {
-        // العثور على الروم بناءً على المعرّف
-        const leaveChannel = guild.channels.cache.get(leaveChannelId);
+    // السيرفر الرسمي الذي تود الإعلان عن الخروج منه
+    const mainServerId = "1122241424197427200";
 
-        if (leaveChannel) {
-            // إرسال رسالة الخروج في الروم المحدد
-            leaveChannel.send(`${officialBot} has left the server.`);
+    // اذا كان السيرفر الذي تمت حذف البوت منه هو السيرفر الرسمي
+    if (guild.id !== mainServerId) {
+        const mainServer = client.guilds.cache.get(mainServerId);
+
+        if (mainServer) {
+            const leaveChannel = mainServer.channels.cache.get(leaveChannelId);
+
+            if (leaveChannel) {
+                leaveChannel.send(`The bot ${officialBot} has left a server: ${guild.name}`);
+            }
         }
     }
 });
+
