7fba4e6d4fe643d84fe1d766cefb72ac73b50cb0
Abdo16j <108068335+Abdo16j@users.noreply.github.com>
2023-06-25 09:26:59 +0100

    Update leavejoin.js

--- src/commands/owner/leavejoin.js
+++ src/commands/owner/leavejoin.js
@@ -2,13 +2,13 @@ client.on('guildCreate', guild => {
     // اسم البوت الرسمي لديك
     const officialBot = "ProControl";
 
-    // معرف الروم في السيرفر الرسمي لإرسال رسالة الدخول
+    // معرف الروم في السيرفر الرئيسي لإرسال رسالة الدخول
     const joinChannelId = "1122241426445578428";
 
-    // السيرفر الرسمي الذي تود الإعلان عن الانضمام إليه
+    // السيرفر الرئيسي الذي تود الإعلان عن الانضمام إليه
     const mainServerId = "1122241424197427200";
 
-    // اذا كان السيرفر الذي تمت دعوة البوت اليه هو السيرفر الرسمي
+    // اذا كان السيرفر الذي تمت دعوة البوت اليه ليس السيرفر الرئيسي
     if (guild.id !== mainServerId) {
         const mainServer = client.guilds.cache.get(mainServerId);
 
@@ -22,27 +22,4 @@ client.on('guildCreate', guild => {
     }
 });
 
-client.on('guildDelete', guild => {
-    // اسم البوت الرسمي لديك
-    const officialBot = "ProControl";
-
-    // معرف الروم في السيرفر الرسمي لإرسال رسالة الخروج
-    const leaveChannelId = "1122241426445578429";
-
-    // السيرفر الرسمي الذي تود الإعلان عن الخروج منه
-    const mainServerId = "1122241424197427200";
-
-    // اذا كان السيرفر الذي تمت حذف البوت منه هو السيرفر الرسمي
-    if (guild.id !== mainServerId) {
-        const mainServer = client.guilds.cache.get(mainServerId);
-
-        if (mainServer) {
-            const leaveChannel = mainServer.channels.cache.get(leaveChannelId);
-
-            if (leaveChannel) {
-                leaveChannel.send(`The bot ${officialBot} has left a server: ${guild.name}`);
-            }
-        }
-    }
-});
 
