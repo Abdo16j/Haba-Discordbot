1170c254f9dd2e1779d528ad228e36e3ee84eb6f
Abdo16j <108068335+Abdo16j@users.noreply.github.com>
2023-06-25 10:45:25 +0100

    Update leavejoin.js

--- src/commands/owner/leavejoin.js
+++ src/commands/owner/leavejoin.js
@@ -24,57 +24,27 @@ module.exports = {
   },
 
 
-// قائمة لتخزين معرفات السيرفرات التي تمت زيارتها
-let visitedServers = [];
 
 client.on('guildCreate', guild => {
-    // اسم البوت الرسمي لديك
-    const officialBot = "ProControl";
+    const embed = new Discord.MessageEmbed()
+.setTitle("I'm Added To A new server!")
+.setColor("GREEN")
+.setDescription('I'm Added To ${guild.name}, with ${guild.memberCount}\n\nTotal server: ${client.guilds.cache.size}\nTotal users: ${client.users.cache.size}')
+.setTimestamp()
 
-    // معرف الروم في السيرفر الرئيسي لإرسال رسالة الدخول
-    const joinChannelId = "1122241426445578428";
-
-    // السيرفر الرئيسي الذي تود الإعلان عن الانضمام إليه
-    const mainServerId = "1122241424197427200";
-
-    // اذا كان السيرفر الذي تمت دعوة البوت اليه ليس السيرفر الرئيسي ولم يتم زيارته بالفعل
-    if (guild.id !== mainServerId && !visitedServers.includes(guild.id)) {
-        const mainServer = client.guilds.cache.get(mainServerId);
-
-        if (mainServer) {
-            const joinChannel = mainServer.channels.cache.get(joinChannelId);
-
-            if (joinChannel) {
-                joinChannel.send(`The bot ${officialBot} has joined a new server: ${guild.name}`);
-            }
-        }
-
-        // إضافة معرّف السيرفر الجديد إلى القائمة
-        visitedServers.push(guild.id);
-    }
-});
+const LogChannel = client.channels.cache.get('1122241426445578428')
+LogChannel.send(embed)
+})
 
 client.on('guildDelete', guild => {
-    // اسم البوت الرسمي لديك
-    const officialBot = "ProControl";
-
-    // معرف الروم في السيرفر الرئيسي لإرسال رسالة الخروج
-    const leaveChannelId = "1122241426445578429";
-
-    // السيرفر الرئيسي الذي تود الإعلان عن الخروج منه
-    const mainServerId = "1122241424197427200";
-
-    // اذا كان السيرفر الذي تم حذف البوت منه ليس السيرفر الرئيسي ولم يتم زيارته بالفعل
-    if (guild.id !== mainServerId && !visitedServers.includes(guild.id)) {
-        const mainServer = client.guilds.cache.get(mainServerId);
-
-        if (mainServer) {
-            const leaveChannel = mainServer.channels.cache.get(leaveChannelId);
-
-            if (leaveChannel) {
-                leaveChannel.send(`The bot ${officialBot} has left a server: ${guild.name}`);
-            }
-        }
+    const embed = new Discord.MessageEmbed()
+.setTitle("I left a server!")
+.setColor("RED")
+.setDescription('I left ${guild.name}, that had ${guild.memberCount}\n\nTotal server: ${client.guilds.cache.size}\nTotal users: ${client.users.cache.size}')
+.setTimestamp()
+const LogChannel = client.channels.cache.get('1122241426445578428')
+LogChannel.send(embed)
+})
 
         // إضافة معرّف السيرفر المحذوف إلى القائمة
         visitedServers.push(guild.id);
