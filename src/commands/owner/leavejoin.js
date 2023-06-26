42678833c1ff69a379f621704072256d25087008
Abdo16j <108068335+Abdo16j@users.noreply.github.com>
2023-06-26 10:53:39 +0100

    Update leavejoin.js

--- src/commands/owner/leavejoin.js
+++ src/commands/owner/leavejoin.js
@@ -19,29 +19,29 @@ module.exports = {
       },
     ],
   },
-
-
+};
 
 client.on('guildCreate', guild => {
-    const embed = new Discord.MessageEmbed()
-.setTitle("I'm Added To A new server!")
-.setColor("GREEN")
-.setDescription('Am Added To ${guild.name}, with ${guild.memberCount}\n\nTotal server: ${client.guilds.cache.size}\nTotal users: ${client.users.cache.size}')
-.setTimestamp()
+  const embed = new Discord.MessageEmbed()
+    .setTitle("I'm Added To A new server!")
+    .setColor("GREEN")
+    .setDescription(`Am Added To ${guild.name}, with ${guild.memberCount}\n\nTotal server: ${client.guilds.cache.size}\nTotal users: ${client.users.cache.size}`)
+    .setTimestamp();
 
-const LogChannel = client.channels.cache.get('1122241426445578428')
-LogChannel.send(embed)
-})
+  const LogChannel = client.channels.cache.get('1122241426445578428');
+  LogChannel.send(embed);
+});
 
 client.on('guildDelete', guild => {
-    const embed = new Discord.MessageEmbed()
-.setTitle("I left a server!")
-.setColor("RED")
-.setDescription('I left ${guild.name}, that had ${guild.memberCount}\n\nTotal server: ${client.guilds.cache.size}\nTotal users: ${client.users.cache.size}')
-.setTimestamp()
-const LogChannel = client.channels.cache.get('1122241426445578428')
-LogChannel.send(embed)
-})
+  const embed = new Discord.MessageEmbed()
+    .setTitle("I left a server!")
+    .setColor("RED")
+    .setDescription(`I left ${guild.name}, that had ${guild.memberCount}\n\nTotal server: ${client.guilds.cache.size}\nTotal users: ${client.users.cache.size}`)
+    .setTimestamp();
 
-        // إضافة معرّف السيرفر المحذوف إلى القائمة
-        visitedServers.push(guild.id);
+  const LogChannel = client.channels.cache.get('1122241426445578428');
+  LogChannel.send(embed);
+});
+
+// إضافة معرّف السيرفر المحذوف إلى القائمة
+visitedServers.push(guild.id);
